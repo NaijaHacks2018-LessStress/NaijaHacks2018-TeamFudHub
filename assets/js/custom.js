e1497a2e047e8ee5e9def66dd559a2d2073082ff
AJ_OJ_SPIN <adjosho@gmail.com>
2018-10-24 11:26:32 +0100

    Modified to load products online
    JS and CSS updates only

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,9 +3,15 @@
 function getallProducts() {
 	console.log("Home");
 
+	// $.getJSON('https://fudhubapi.dynamicbra.in/api/product/getall',  function(data) {
+ //        	console.log(data);
+           
+           
+ //    });
+
 	$.ajax({
 		url: 'https://fudhubapi.dynamicbra.in/api/product/getall', 
-		// dataType: 'json',
+		dataType: 'json',
 		type: 'GET',
 		headers: {
 			'contentType': 'application/json',
@@ -17,7 +23,32 @@ function getallProducts() {
 
       success: function(data) {
         // alert("come");
-         console.log(data);
+        console.log(data);
+
+        var content = $.parseJSON(data);
+        if(content['ResponseCode'] == 1) {
+        	var row = content['Data'];
+        	var products = "";
+        	$.each(row, function(index, element) {
+			    alert(element.timeStamp); 
+			    products = products + '<div class="col-md-4" data-product="' + element.ID + '"><div class="thumb-product">';
+			    products = products + '<img class="thumb-product-img" src="assets/img/thumb/08.jpg" alt="">';
+			    products = products + '<div class="thumb-product-in"><h4><a href="shop-ui-inner.html">'+ element.Title +'</a> –';
+			    products = products + ' <a href="shop-ui-inner.html">'+ element.Location +'</a></h4>';
+			    products = products + '<p class="text-sm text-info text-center">'+ element.Description + '</p>';
+			    products = products + '<span class="thumb-product-type">'+ element.Category.Name +'</span></div>';
+			    products = products + '<ul class="list-inline overflow-h">';
+			    products = products + '<li class="thumb-product-price">NGN'+ element.Amount +'</li>';
+			    products = products + '<li class="thumb-product-purchase"><a href="#"><i class="fa fa-shopping-cart"></i></a>';
+			    products = products + ' | <a href="#"><i class="fa fa-heart"></i></a></li></ul></div></div>';
+			});
+
+        	$("#products-list").hide('fast');
+			$("#product-list").html(products);
+        	$("#products-list").show('slow');
+
+        }
+
       },
       error: function(data) {
       	// alert("failed");
@@ -29,5 +60,6 @@ function getallProducts() {
 
 
 function uploadfoodstuff() {
-	
+
+
 }
